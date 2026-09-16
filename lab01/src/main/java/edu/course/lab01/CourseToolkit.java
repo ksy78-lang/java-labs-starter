a06cb1d95a01ca28850a7fb0d29ee6524904034b
ksy78-lang <ksylybim78@gmail.com>
2026-09-16 21:55:44 +0300

    Лаба 1 Телицына

--- lab01/src/main/java/edu/course/lab01/CourseToolkit.java
+++ lab01/src/main/java/edu/course/lab01/CourseToolkit.java
@@ -1,5 +1,4 @@
 package edu.course.lab01;
-
 /**
  * Небольшие методы для первой лабораторной работы.
  */
@@ -8,11 +7,23 @@ public final class CourseToolkit {
     private CourseToolkit() {
         // Утилитарный класс не должен иметь экземпляров.
     }
-
+   
+    
     /**
      * Возвращает true, если число четное.
      */
     public static boolean isEven(int number) {
         return number % 2 == 0;
     }
+     public static boolean isPrime(int number){
+        if (number<2){
+            return false;
+        }
+        for (int i=2; i*i<=number;i++){
+            if (number%i==0){
+                return false;
+            }
+        }
+        return true;
+        }
 }
